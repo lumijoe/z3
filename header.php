9bf7284a0cd7fce341228e96f5c1b0e9cddec425
小城 <kojolumi@gamil.com>
2024-12-17 14:41:46 +0900

    test

--- header.php
+++ header.php
@@ -19,9 +19,50 @@
 
 <body>
     <header class="l-header">
-        <section class="header-inner">
-            <h1 class="header-logo"><a href="<?php echo esc_url(home_url()); ?>" style="color:#fff!important;">logo</a></h1>
-            <nav class="header-nav c-flexcol">
+        <section class="l-header-inner">
+            <div class="l-header-inner--level1" >
+                <div class="l-header-inner--level1-left">
+                    <p style="font-size:12px;">熱技術の新たな次元へ</p>
+                </div>
+                <div class="l-header-inner--level1-right">
+                    <ul class="header-right">
+                        <li class="icon-set">
+                            <i class="icon1"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_add_circle.svg" alt=""></i>
+                            <i class="icon2"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_add_circle.svg" alt=""></i>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" target="_blank" class="btn-round">
+                                <i class="fas fa-users" style="font-size:12px;"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_add_circle.svg" alt=""></i>
+                                採用情報
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" target="_blank" class="btn-round">
+                                <i class="fas fa-users" style="font-size:12px;"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_add_circle.svg" alt=""></i>
+                                資料ダウンロード
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" target="_blank" class="btn-round">
+                                <i class="fas fa-users" style="font-size:12px;"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_add_circle.svg" alt=""></i>
+                                お問い合わせ一覧
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- header-level2 -->
+            <div class="l-header-inner--level2">プラントトップ</div>
+
+
+
+
+
+
+            <!-- テスト中 -->
+            <!-- <section class="header-inner">
+                <h1 class="header-logo"><a href="<?php echo esc_url(home_url()); ?>" style="color:#fff!important;">logo</a></h1>
+                <nav class="header-nav c-flexcol">
                 <ul class="header-nav--first c-flexrow" style="gap: 20px;">
                     <li style="color:#fff!important;"><a href="<?php echo esc_url(home_url('/products01#01search')); ?>" style="color:#fff!important;">検索A</a></li>
                     <li style="color:#fff!important;"><a href="<?php echo esc_url(home_url('/products02#02search')); ?>" style="color:#fff!important;">検索B</a></li>
@@ -32,7 +73,7 @@
                     <li style="color:#fff;"><a href="<?php echo esc_url(home_url('/newslist-sustainability')); ?>" style="color:#fff!important;">サステナビリティニュース</a></li>
                     <li style="color:#fff;"><a href="<?php echo esc_url(home_url('/newslist-ir')); ?>" style="color:#fff!important;">IRニュース</a></li>
                 </ul>
-            </nav>
-        </section>
+            </nav>-->
+        </section> 
     </header>
     <main>
